f2c68ff89c422c0610ed6f1a37fadf3a16b90059
Striking <Strikingwolf2012@gmail.com>
2015-09-13 20:45:41 -0500

    Basic logic, tests and further impl will come later

---
+++ src/geometry/logic.clj
@@ -0,0 +1,43 @@
+(defprotocol LogicAtom
+  (not [this])
+  (? [this given]))
+
+(defprotocol LogicStatement
+  (compose? [this thing])
+  (? [this given])
+  (converse [this])
+  (inverse [this])
+  (contrapositive [this])
+  (converse-inverse? [this])
+  (not [this]))
+
+(defrecord NotAtom [thing]
+  LogicAtom
+  (not [this]
+    (Atom. thing))
+  (? [this given]
+    (? (not this) given)))
+
+(defrecord Atom [thing]
+  LogicAtom
+  (not [this]
+    (NotAtom. thing)
+  (? [this given])
+    "Not implemented")
+
+(defrecord If [statement conclusion]
+  LogicStatement
+  (compose? [this thing]
+    "Not implemented")
+  (? [this given]
+    "Not implemented")
+  (converse [this]
+    (If. conclusion statement))
+  (inverse [this]
+    (If. (not conclusion) (not statement)))
+  (contrapositive [this]
+    (If. (not statement) (not conclusion)))
+  (converse-inverse [this]
+    :nnt)
+  (not [this]
+    (If. statement (not conclusion))))
